d0852be4f4e41e5b2249b8d5c3c584988bfad9ba
Thilakshi Mehensaperera <thilakshimehensa@gmail.com>
2026-05-15 16:50:35 +0530

    create default,parameterize constructor

--- src/main/java/com/example/hometutor/model/Review.java
+++ src/main/java/com/example/hometutor/model/Review.java
@@ -32,7 +32,64 @@ public abstract class Review implements IdentifiableEntity {
     private String comment;
 
     //default constructor
+    protected Review() {
+    }
+//pararamterize constructor
 
+  protected Review(String id, String tutorId, String userId, int rating, String comment) {
+    this.id = id;
+    this.tutorId = tutorId;
+    this.userId = userId;
+    this.rating = rating;
+    this.comment = comment;
+  }
+  public abstract String getReviewType();
+
+  public String displayReview() {
+    return rating + "/5 - " + comment;
+  }
+
+  @Override
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getTutorId() {
+    return tutorId;
+  }
+
+  public void setTutorId(String tutorId) {
+    this.tutorId = tutorId;
+  }
+
+  public String getUserId() {
+    return userId;
+  }
+
+  public void setUserId(String userId) {
+    this.userId = userId;
+  }
+
+  public int getRating() {
+    return rating;
+  }
+
+  public void setRating(int rating) {
+    this.rating = rating;
+  }
+
+  public String getComment() {
+    return comment;
+  }
+
+  public void setComment(String comment) {
+    this.comment = comment;
+  }
+}
 
 
 

8db7f6530c9b4791129b083d01acd44e6e7d998b
Thilakshi Mehensaperera <thilakshimehensa@gmail.com>
2026-05-15 13:29:01 +0530

    create a class

--- src/main/java/com/example/hometutor/controller/PaymentController.java
+++ src/main/java/com/example/hometutor/controller/PaymentController.java
@@ -1,4 +1,31 @@
 //create a package
 package com.example.hometutor.controller;
 
+import com.example.hometutor.model.Payment;
+import com.example.hometutor.service.PaymentService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
+@RequestMapping("/payments")
+//create a class
+public class PaymentController {
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

861132c7e00670dd7f435869ee89179818368f3f
Thilakshi Mehensaperera <thilakshimehensa@gmail.com>
2026-05-15 16:54:31 +0530

    getter and setters

--- src/main/java/com/example/hometutor/model/Review.java
+++ src/main/java/com/example/hometutor/model/Review.java
@@ -43,61 +43,62 @@ public abstract class Review implements IdentifiableEntity {
     this.rating = rating;
     this.comment = comment;
   }
+  //methods
   public abstract String getReviewType();
 
   public String displayReview() {
+
     return rating + "/5 - " + comment;
   }
-
+//getter & setters
   @Override
   public String getId() {
+
     return id;
   }
 
   public void setId(String id) {
+
     this.id = id;
   }
 
   public String getTutorId() {
+
     return tutorId;
   }
 
   public void setTutorId(String tutorId) {
+
     this.tutorId = tutorId;
   }
 
   public String getUserId() {
+
     return userId;
   }
 
   public void setUserId(String userId) {
+
     this.userId = userId;
   }
 
   public int getRating() {
+
     return rating;
   }
 
   public void setRating(int rating) {
+
     this.rating = rating;
   }
 
   public String getComment() {
+
     return comment;
   }
 
   public void setComment(String comment) {
+
     this.comment = comment;
   }
-}
-
-
-
-
-
-
-
-
-
-
 }

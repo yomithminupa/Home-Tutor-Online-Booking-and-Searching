a8ae7b2cac49ece6158c4460194a208f7425b09e
Thilakshi Mehensaperera <thilakshimehensa@gmail.com>
2026-05-16 21:19:44 +0530

    create a class

--- src/main/java/com/example/hometutor/model/PublicReview.java
+++ src/main/java/com/example/hometutor/model/PublicReview.java
@@ -3,3 +3,25 @@ package com.example.hometutor.model;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+
+//create a class
+public class PublicReview extends Review {
+   //attributes
+   private String nickname;
+//default constructor
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

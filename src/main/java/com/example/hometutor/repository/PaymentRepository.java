db41ba5ba6fae66df001728ad53ac527a953013e
Thilakshi Mehensaperera <thilakshimehensa@gmail.com>
2026-05-17 01:29:29 +0530

    start the PaymentRepository

--- src/main/java/com/example/hometutor/repository/PaymentRepository.java
+++ src/main/java/com/example/hometutor/repository/PaymentRepository.java
@@ -1,7 +1,5 @@
+//create a package
 package com.example.hometutor.repository;
 
 import com.example.hometutor.model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, String> {
-}

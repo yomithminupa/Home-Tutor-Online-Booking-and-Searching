a3681600339c21570118ab0cf8bc94fd5ead227a
Thilakshi Mehensaperera <thilakshimehensa@gmail.com>
2026-05-16 21:18:12 +0530

    start the PublicReview

--- src/main/java/com/example/hometutor/model/PublicReview.java
+++ src/main/java/com/example/hometutor/model/PublicReview.java
@@ -1,36 +1,5 @@
+//create a package
 package com.example.hometutor.model;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
-
-@Entity
-@DiscriminatorValue("PUBLIC")
-public class PublicReview extends Review {
-    private String nickname;
-
-    public PublicReview() {
-    }
-
-    public PublicReview(String id, String tutorId, String userId, int rating, String comment, String nickname) {
-        super(id, tutorId, userId, rating, comment);
-        this.nickname = nickname;
-    }
-
-    @Override
-    public String getReviewType() {
-        return "PUBLIC";
-    }
-
-    @Override
-    public String displayReview() {
-        return nickname + ": " + super.displayReview();
-    }
-
-    public String getNickname() {
-        return nickname;
-    }
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
-}

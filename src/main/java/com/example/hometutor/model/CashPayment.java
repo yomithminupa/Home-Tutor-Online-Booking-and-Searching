0d7c05a643bd74044833c1be2de2ba06486efa0a
Thilakshi Mehensaperera <thilakshimehensa@gmail.com>
2026-05-16 18:05:12 +0530

    creat attributes

--- src/main/java/com/example/hometutor/model/CashPayment.java
+++ src/main/java/com/example/hometutor/model/CashPayment.java
@@ -7,3 +7,52 @@ import jakarta.persistence.Entity;
 @Entity
 @DiscriminatorValue("CASH")
 //create a class
+public class CashPayment extends Payment {
+    //attributes
+    private String collectedBy;
+    private String receiptNumber;
+    //default constructor
+    public CashPayment() {
+    }
+    //parameterize constructor
+    public CashPayment(String id, String bookingId, String userId, double amount, String paymentDate, String status,
+                       String collectedBy, String receiptNumber) {
+        super(id, bookingId, userId, amount, paymentDate, status);
+        this.collectedBy = collectedBy;
+        this.receiptNumber = receiptNumber;
+    }
+    //getters and setters , method overriding
+    @Override
+    public String getPaymentMethod() {
+
+        return "Cash Payment";
+    }
+
+    public String getCollectedBy() {
+
+        return collectedBy;
+    }
+
+    public void setCollectedBy(String collectedBy) {
+
+        this.collectedBy = collectedBy;
+    }
+
+    public String getReceiptNumber() {
+
+        return receiptNumber;
+    }
+
+    public void setReceiptNumber(String receiptNumber) {
+
+        this.receiptNumber = receiptNumber;
+    }
+}
+
+
+
+
+
+
+
+}

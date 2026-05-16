01d2b7c25fc1adc307f1332b40994cc703ffacef
Thilakshi Mehensaperera <thilakshimehensa@gmail.com>
2026-05-16 18:01:43 +0530

    start cashPayment file

--- src/main/java/com/example/hometutor/model/CashPayment.java
+++ src/main/java/com/example/hometutor/model/CashPayment.java
@@ -1,3 +1,4 @@
+//create a  package
 package com.example.hometutor.model;
 
 import jakarta.persistence.DiscriminatorValue;
@@ -5,38 +6,4 @@ import jakarta.persistence.Entity;
 
 @Entity
 @DiscriminatorValue("CASH")
-public class CashPayment extends Payment {
-    private String collectedBy;
-    private String receiptNumber;
-
-    public CashPayment() {
-    }
-
-    public CashPayment(String id, String bookingId, String userId, double amount, String paymentDate, String status,
-                       String collectedBy, String receiptNumber) {
-        super(id, bookingId, userId, amount, paymentDate, status);
-        this.collectedBy = collectedBy;
-        this.receiptNumber = receiptNumber;
-    }
-
-    @Override
-    public String getPaymentMethod() {
-        return "Cash Payment";
-    }
-
-    public String getCollectedBy() {
-        return collectedBy;
-    }
-
-    public void setCollectedBy(String collectedBy) {
-        this.collectedBy = collectedBy;
-    }
-
-    public String getReceiptNumber() {
-        return receiptNumber;
-    }
-
-    public void setReceiptNumber(String receiptNumber) {
-        this.receiptNumber = receiptNumber;
-    }
-}
+//create a class

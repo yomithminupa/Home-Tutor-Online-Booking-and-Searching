efeb31325d434c12042ac93aa2f454c349e483e1
Thilakshi Mehensaperera <thilakshimehensa@gmail.com>
2026-05-17 00:05:22 +0530

    start the VerifiedReview

--- src/main/java/com/example/hometutor/model/VerifiedReview.java
+++ src/main/java/com/example/hometutor/model/VerifiedReview.java
@@ -1,3 +1,4 @@
+//create a package
 package com.example.hometutor.model;
 
 import jakarta.persistence.DiscriminatorValue;
@@ -5,32 +6,10 @@ import jakarta.persistence.Entity;
 
 @Entity
 @DiscriminatorValue("VERIFIED")
+//create a class
 public class VerifiedReview extends Review {
-    private String bookingId;
 
-    public VerifiedReview() {
-    }
 
-    public VerifiedReview(String id, String tutorId, String userId, int rating, String comment, String bookingId) {
-        super(id, tutorId, userId, rating, comment);
-        this.bookingId = bookingId;
-    }
 
-    @Override
-    public String getReviewType() {
-        return "VERIFIED";
-    }
 
-    @Override
-    public String displayReview() {
-        return "Verified booking " + bookingId + ": " + super.displayReview();
-    }
-
-    public String getBookingId() {
-        return bookingId;
-    }
-
-    public void setBookingId(String bookingId) {
-        this.bookingId = bookingId;
-    }
 }

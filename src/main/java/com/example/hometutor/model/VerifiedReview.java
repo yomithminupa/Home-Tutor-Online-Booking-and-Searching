6937a09a88efcc4db02095cffb288e53bf7152b2
Thilakshi Mehensaperera <thilakshimehensa@gmail.com>
2026-05-17 00:20:34 +0530

    create attributes

--- src/main/java/com/example/hometutor/model/VerifiedReview.java
+++ src/main/java/com/example/hometutor/model/VerifiedReview.java
@@ -11,34 +11,37 @@ import jakarta.persistence.Entity;
 public class VerifiedReview extends Review {
     //attributes
     private String bookingId;
- //default constructor
- public VerifiedReview() {
- }
+
+    //default constructor
+    public VerifiedReview() {
+    }
+
     //parameterize constructor
     public VerifiedReview(String id, String tutorId, String userId, int rating, String comment, String bookingId) {
         super(id, tutorId, userId, rating, comment);
         this.bookingId = bookingId;
     }
+
     //method overriding and getters and setters
     @Override
     public String getReviewType() {
 
-     return "VERIFIED";
+        return "VERIFIED";
     }
 
     @Override
     public String displayReview() {
 
-     return "Verified booking " + bookingId + ": " + super.displayReview();
+        return "Verified booking " + bookingId + ": " + super.displayReview();
     }
 
     public String getBookingId() {
 
-     return bookingId;
+        return bookingId;
     }
 
     public void setBookingId(String bookingId) {
 
-     this.bookingId = bookingId;
+        this.bookingId = bookingId;
     }
 }

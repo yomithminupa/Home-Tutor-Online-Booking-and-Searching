68aebfd16f8c5aa6fb77730aff767bf1840a56bd
Thilakshi Mehensaperera <thilakshimehensa@gmail.com>
2026-05-17 01:30:38 +0530

    create a class & finished

--- src/main/java/com/example/hometutor/repository/PaymentRepository.java
+++ src/main/java/com/example/hometutor/repository/PaymentRepository.java
@@ -3,3 +3,8 @@ package com.example.hometutor.repository;
 
 import com.example.hometutor.model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+//create a class
+public interface PaymentRepository extends JpaRepository<Payment, String> {
+
+}

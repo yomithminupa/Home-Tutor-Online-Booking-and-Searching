08db816e8c4ac39e1317a1865751b839f56c69f7
Thilakshi Mehensaperera <thilakshimehensa@gmail.com>
2026-05-16 12:34:45 +0530

    create a class

--- src/main/java/com/example/hometutor/model/CardPayment.java
+++ src/main/java/com/example/hometutor/model/CardPayment.java
@@ -4,6 +4,33 @@ package com.example.hometutor.model;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 
+@Entity
+@DiscriminatorValue("CARD")
+//create a class
+public class CardPayment extends Payment{
+   //attributes
+   private String cardHolderName;
+    private String maskedCardNumber;
+    private String authorizationCode;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
 
 
 

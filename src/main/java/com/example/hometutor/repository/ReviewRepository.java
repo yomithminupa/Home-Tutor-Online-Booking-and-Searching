0390dedadcf70d49c411cb06dcdc93a7de77c9c3
Thilakshi Mehensaperera <thilakshimehensa@gmail.com>
2026-05-17 01:42:27 +0530

    Start the ReviewRepository

--- src/main/java/com/example/hometutor/repository/ReviewRepository.java
+++ src/main/java/com/example/hometutor/repository/ReviewRepository.java
@@ -1,7 +1,2 @@
+//create a package
 package com.example.hometutor.repository;
-
-import com.example.hometutor.model.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<Review, String> {
-}

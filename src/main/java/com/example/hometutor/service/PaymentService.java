ca45b8224849ab6753e1e66f515fc3a3f84918f5
Thilakshi Mehensaperera <thilakshimehensa@gmail.com>
2026-05-17 08:26:54 +0530

    create a class

--- src/main/java/com/example/hometutor/service/PaymentService.java
+++ src/main/java/com/example/hometutor/service/PaymentService.java
@@ -1,2 +1,14 @@
 //create a package
 package com.example.hometutor.service;
+
+import com.example.hometutor.model.CardPayment;
+import com.example.hometutor.model.CashPayment;
+import com.example.hometutor.model.Payment;
+import com.example.hometutor.repository.PaymentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Locale;
+
+@Service
+//create a class

02c7c823e111e7b034d05249a890caabe9699c3d
Thilakshi Mehensaperera <thilakshimehensa@gmail.com>
2026-05-17 10:18:07 +0530

    creates another method

--- src/main/java/com/example/hometutor/service/ReviewService.java
+++ src/main/java/com/example/hometutor/service/ReviewService.java
@@ -26,7 +26,7 @@ public ReviewService(ReviewRepository reviewRepository) {
         }
         return new PublicReview(id, tutorId, userId, rating, comment, nickname);
     }
-    //method overriding'l
+    //method overridingk
     @Override
     public List<Review> search(String keyword) {
         if (keyword == null || keyword.isBlank()) {

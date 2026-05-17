4dff6227c7612e3fe390caf67c9edb8459f42c38
hiranthadulanjana24-stack <hiranthadulanjana24@gmail.com>
2026-05-17 22:28:44 +0530

    Update SubjectRepository.java

--- SubjectRepository.java
+++ SubjectRepository.java
@@ -4,4 +4,6 @@ import com.example.hometutor.model.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SubjectRepository extends JpaRepository<Subject, String> {
+  // Find subjects by name
+    List<Subject> findByName(String name);
 }

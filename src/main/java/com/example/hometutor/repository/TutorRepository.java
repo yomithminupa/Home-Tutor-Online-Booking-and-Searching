ef70f2943df629170d80b00284d9a4484837d18e
Budmin Gunasekara <budmingunasekara369@gmail.com>
2026-05-17 22:34:34 +0530

    Update TutorRepository.java

--- src/main/java/com/example/hometutor/repository/TutorRepository.java
+++ src/main/java/com/example/hometutor/repository/TutorRepository.java
@@ -3,5 +3,6 @@ package com.example.hometutor.repository;
 import com.example.hometutor.model.Tutor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TutorRepository extends JpaRepository<Tutor, String> {
+public interface TutorRepository extends JpaRepository<Tutor, String> 
+{ 
 }

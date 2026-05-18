c90b8582e688a9cba9ace15ca2a8175c4fc0dc59
Budmin Gunasekara <budmingunasekara369@gmail.com>
2026-05-18 22:45:16 +0530

    Update Tutor.java

--- src/main/java/com/example/hometutor/model/Tutor.java
+++ src/main/java/com/example/hometutor/model/Tutor.java
@@ -1,6 +1,6 @@
-package com.example.hometutor.model; 
+package com.example.hometutor.model; // Defines the package location of the class
  
-import jakarta.persistence.Column;
+import jakarta.persistence.Column;  // These JPA annotations help to connect to java classes with database tables.
 import jakarta.persistence.DiscriminatorColumn;
 import jakarta.persistence.DiscriminatorType;
 import jakarta.persistence.Entity;
@@ -9,7 +9,8 @@ import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import jakarta.persistence.Table;
 
-@Entity   
+@Entity   // Marks this class as a database entity
+ 
 @Table(name = "tutors") 
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "tutor_type", discriminatorType = DiscriminatorType.STRING)

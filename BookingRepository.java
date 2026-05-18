5c6f5558cd3d0f2a499673e827c49bde6da743f7
Poornima Gunarathne <poornimagunarathne79@gmail.com>
2026-05-18 15:19:35 +0530

    Update BookingRepository.java

--- BookingRepository.java
+++ BookingRepository.java
@@ -1,7 +1,9 @@
 //IT25102587
 package com.example.hometutor.repository;
 
+// Import Booking entity class
 import com.example.hometutor.model.Booking;
+// Import JpaRepository interface
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookingRepository extends JpaRepository<Booking, String> {

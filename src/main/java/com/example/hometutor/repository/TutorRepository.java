23af0e7fb7c133eba0de02ba64ca02d6e4acc54d
D.T.M.perera <thilakshimehensa@gmail.com>
2026-05-18 14:24:42 +0530

    delete file

--- src/main/java/com/example/hometutor/repository/TutorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.hometutor.repository;
-
-import com.example.hometutor.model.Tutor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TutorRepository extends JpaRepository<Tutor, String> {
-}
